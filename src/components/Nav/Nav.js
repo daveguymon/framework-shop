967647b6421c5318c52209d5ae6829665d64f243
Dave Guymon <davidwguymon@gmail.com>
2017-07-07 15:57:08 -0600

    Completed Step 5

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import "./Nav.css";
 import javascriptLogo from "../../assets/javascript.svg";
+import { Link } from 'react-router-dom';
 
 export function Nav( { cartTotal } ) {
 	return (
 		<nav className="nav">
-			<div className="nav__header-wrapper">
+			
+        <Link to="/">
+        <div className="nav__header-wrapper">
 					<img
 						alt="javascript logo"
 						className="nav__javascript-logo"
@@ -17,7 +19,11 @@ export function Nav( { cartTotal } ) {
 						The JavaScript Framework Shop
 					</h3>
 			</div>
-			<p className="nav__cart">Cart ( ${ /* total price of products in cart */ } )</p>
+        </Link>
+        
+        <Link to="/cart">
+			<p className="nav__cart">Cart ( ${ cartTotal } )</p>
+        </Link>
 		</nav>
 	);
 }
